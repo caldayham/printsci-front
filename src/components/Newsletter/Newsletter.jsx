b25c1405287648a6ce96955396c85e29d5f4f9f9
caldayham <caldayham@gmail.com>
2022-09-13 09:01:34 -0700

    working on newsletter functionality

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Title,
@@ -10,12 +10,19 @@ import {
 import SendIcon from "@mui/icons-material/Send";
 
 const Newsletter = () => {
+
+  const [boxMessage, changeBoxMessage] = useState("Your email here");
+
+  function handleEmail() {
+    changeBoxMessage("Thank you")
+  }
+
   return (
     <Container>
       <Title>Newsletter</Title>
       <Desc>Constantly improving. We never spam email.</Desc>
       <InputContainer>
-        <Input placeholder="Your email" />
+        <Input placeholder={boxMessage} onChange={() => handleEmail()}/>
         <Button>
           <SendIcon />
         </Button>
